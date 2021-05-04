6a16f74e61ce997d918628e6013b2c48dff96527
helionporto <helionporto@hotmail.com>
2021-05-03 21:37:18 -0400

    Project created

---
+++ backend/src/test/java/com/helion/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helion.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
